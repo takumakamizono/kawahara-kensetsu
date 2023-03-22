a8f19a0bd202ae7a3beec42263c171c49a91bb4f
神園巧真 <>
2023-03-22 09:54:37 +0900

    sliderの動きをフェードに変更等動きを変えました。

--- kawahara-kensetsu/scripts/libs/hero-slider.js
+++ kawahara-kensetsu/scripts/libs/hero-slider.js
@@ -1,37 +1,35 @@
 class HeroSlider {
-    constructor(el) {
-        this.el = el;
-        this.swiper = this._initSwiper();
-    }
+  constructor(el) {
+    this.el = el;
+    this.swiper = this._initSwiper();
+  }
 
-    _initSwiper() {
-        return new Swiper(this.el, {
-            // Optional parameters
-            // direction: 'vertical',
-            loop: true,
-            grabCursor: true,
-            effect: 'coverflow',
-            centeredSlides: true,
-            slidesPerView: 1,
-            speed: 1000,
-            breakpoints: {
-                1024: {
-                    slidesPerView: 2,
-                }
-            },
-        });
-    }
+  _initSwiper() {
+    return new Swiper(this.el, {
+      // Optional parameters
+      // direction: 'vertical',
+      loop: true,
+      grabCursor: true,
+      effect: "fade",
+      centeredSlides: true,
+      slidesPerView: 1,
+      speed: 1000,
+    });
+  }
 
-    start(options = {}) {
-        options = Object.assign({
-            delay: 4000,
-            disableOnInteraction: false
-        }, options);
-        
-        this.swiper.params.autoplay = options;
-        this.swiper.autoplay.start();
-    }
-    stop() {
-        this.swiper.autoplay.stop();
-    }
+  start(options = {}) {
+    options = Object.assign(
+      {
+        delay: 4000,
+        disableOnInteraction: false,
+      },
+      options
+    );
+
+    this.swiper.params.autoplay = options;
+    this.swiper.autoplay.start();
+  }
+  stop() {
+    this.swiper.autoplay.stop();
+  }
 }
