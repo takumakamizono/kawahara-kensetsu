14e277bc472077d88f1cb6bcee6f92ec491e70fb
神園巧真 <>
2023-03-22 17:11:59 +0900

    horo部分をパーツ化しインクルードしました。

---
+++ kawahara-kensetsu/include/hero.php
@@ -0,0 +1,22 @@
+<div class="hero">
+            <div class="swiper">
+              <div class="swiper-wrapper">
+                <div class="swiper-slide">
+                  <div class="hero__title">Enjoy Rich</div>
+                  <img src="<?= get_template_directory_uri(); ?>/images/slider.png" alt="" />
+                </div>
+                <div class="swiper-slide">
+                  <div class="hero__title">Fantastic</div>
+                  <img src="<?= get_template_directory_uri(); ?>/images/slider.png" alt="" />
+                </div>
+                <div class="swiper-slide">
+                  <div class="hero__title">Experience</div>
+                  <img src="<?= get_template_directory_uri(); ?>/images/slider.png" alt="" />
+                </div>
+              </div>
+              <div class="hero__footer">
+                <img class="hero__downarrow" src="<?= get_template_directory_uri(); ?>/images/arrow.svg" />
+                <span class="hero__scrolltext">scroll</span>
+              </div>
+            </div>
+          </div>
--- kawahara-kensetsu/index.php
+++ kawahara-kensetsu/index.php
@@ -12,28 +12,7 @@
         <?php get_template_part('include/header'); ?>
         </header>
         <div id="content">
-          <div class="hero">
-            <div class="swiper">
-              <div class="swiper-wrapper">
-                <div class="swiper-slide">
-                  <div class="hero__title">Enjoy Rich</div>
-                  <img src="<?= get_template_directory_uri(); ?>/images/image-1.jpg" alt="" />
-                </div>
-                <div class="swiper-slide">
-                  <div class="hero__title">Fantastic</div>
-                  <img src="<?= get_template_directory_uri(); ?>/images/image-2.jpg" alt="" />
-                </div>
-                <div class="swiper-slide">
-                  <div class="hero__title">Experience</div>
-                  <img src="<?= get_template_directory_uri(); ?>/images/image-3.jpg" alt="" />
-                </div>
-              </div>
-              <div class="hero__footer">
-                <img class="hero__downarrow" src="<?= get_template_directory_uri(); ?>/images/arrow.svg" />
-                <span class="hero__scrolltext">scroll</span>
-              </div>
-            </div>
-          </div>
+        <?php get_template_part('include/hero'); ?>
           <div id="main-content">
             <aside class="side left">
               <div class="side__inner">
