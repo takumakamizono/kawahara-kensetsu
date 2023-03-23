6cb1df008e2762a633ea7e4429a6720af7014b54
神園巧真 <>
2023-03-23 13:16:30 +0900

    conpanyセクションのコンポーネント化をしました。

---
+++ kawahara-kensetsu/include/company.php
@@ -0,0 +1,25 @@
+<section class="company">
+                <div class="company__inner">
+                  <div class="company__img cover-slide">
+                    <img class="img-zoom" src="<?= get_template_directory_uri(); ?>/images/company01.png" />
+                  </div>
+                  <div class="company__texts appear left">
+                    <div class="company__texts-inner">
+                      <div class="company__title main-title item">
+                        COMPANY
+                      </div>
+                      <div class="company__sub sub-title item">人とのつながりを大事に</div>
+                      <div class="company__description item">
+                        <p>
+                        地域の発展に寄与できる<br />企業を目指して
+                        </p>
+                       
+                      </div>
+                      <div class="company__btn item">
+                        <a class="btn02" href="">more</a>
+                      
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </section>
