e78712b8995a21b9cccf59323974cf33c9d693e0
神園巧真 <>
2023-03-23 13:19:15 +0900

    サイドセクションを右左それぞれコンポーネント化

---
+++ kawahara-kensetsu/include/side-left.php
@@ -0,0 +1,5 @@
+<aside class="side left">
+              <div class="side__inner">
+                <a href="#" class="twitter icon tween-animate-title">Instagram</a>
+              </div>
+            </aside>
