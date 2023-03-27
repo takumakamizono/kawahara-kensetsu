5c92d09b89d84c1fb42539e3b641ea0ddbdd445c
神園巧真 <>
2023-03-27 10:29:23 +0900

    お問い合わせ完了後完了ページに遷移する設定

--- kawahara-kensetsu/functions.php
+++ kawahara-kensetsu/functions.php
@@ -87,3 +87,25 @@ function custom_wpcf7_validate_kana($result,$tag)
 }
 add_filter('wpcf7_validate_text', 'custom_wpcf7_validate_kana', 11, 2);
 add_filter('wpcf7_validate_text*', 'custom_wpcf7_validate_kana', 11, 2);
+
+//お問い合わせと送信完了（固定ページ）のスラッグをセットする
+$contact = 'contact';
+$thanks = 'thanks';
+
+//お問い合わせフォームの送信後にサンクスページへ飛ばす
+add_action( 'wp_footer', 'redirect_thanks_page' );
+function redirect_thanks_page() {
+  global $contact;
+  global $thanks;
+  ?>
+  <?php if( is_page($contact)):?>
+  <script>
+    document.addEventListener( 'wpcf7mailsent', function( event ) {
+      location = '<?php echo home_url('/'.$thanks); ?>'; // 遷移先のURL
+    }, false );
+  </script>
+<?php endif; ?>
+
+<?php
+
+}
