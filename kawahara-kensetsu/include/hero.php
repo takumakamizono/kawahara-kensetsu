4f8d33eff558ee6d879f359afc26d12e66ce2cd8
神園巧真 <>
2023-03-28 14:18:11 +0900

    下層ページのヘッダー画像の変更。投稿のアーカイブページの個別ページのタイトルの表示設定。 記事がないときのタイトルの表示設定をしました。

--- kawahara-kensetsu/include/hero.php
+++ kawahara-kensetsu/include/hero.php
@@ -5,57 +5,99 @@
                 <div class="swiper-slide">
                   <div class="hero__title">美しい天降川や霧島の山々<br>そこに住む人々の環境を守りながら<br>皆さまとともに歩み続けます
                 </div>
-                  <img src="<?= get_template_directory_uri(); ?>/images/slider.png" alt="" />
+                  <img src="<?= get_template_directory_uri(); ?>/images/top-image01.png" alt="" />
                 </div>
                 <div class="swiper-slide">
                   <div class="hero__title">美しい天降川や霧島の山々<br>そこに住む人々の環境を守りながら<br>皆さまとともに歩み続けます</div>
-                  <img src="<?= get_template_directory_uri(); ?>/images/slider.png" alt="" />
+                  <img src="<?= get_template_directory_uri(); ?>/images/top-image01.png" alt="" />
                 </div>
                 <div class="swiper-slide">
                   <div class="hero__title">美しい天降川や霧島の山々<br>そこに住む人々の環境を守りながら<br>皆さまとともに歩み続けます</div>
-                  <img src="<?= get_template_directory_uri(); ?>/images/slider.png" alt="" />
+                  <img src="<?= get_template_directory_uri(); ?>/images/top-image01.png" alt="" />
                 </div>
               </div>
-              <div class="hero__footer">
-                <img class="arrow-downarrow" src="<?= get_template_directory_uri(); ?>/images/arrow.svg" />
-                <span class="arrow-scrolltext">scroll</span>
-              </div>
+             
             </div>
           </div>
-   <?php elseif(is_single() ||  is_archive()):?>
+   <?php elseif(is_archive()):?>
     <?php 
-$category = get_the_category();
-$cat_name = $category[0]->cat_name;
-$category_nicename = $category[0]->category_nicename;
+    $category = get_queried_object();
+    $cat_slug = $category->category_nicename;
+    $cat_name = $category->cat_name;
 ?>
       <div class="hero-sub">    
         <div class="hero-sub__inner">
-              <div class="hero-sub__titles">
-              <h2 class="hero-sub__maintitle "><?=  esc_html($category_nicename); ?></h2>
-              <p class="hero-sub__subtitle"><?=  esc_html($cat_name); ?></p>
+              <div class="hero-sub__titles">               
+              <h2 class="hero-sub__maintitle "><?=  esc_html($cat_slug); ?></h2>
+              <p class="hero-sub__subtitle"><?=  esc_html($cat_name); ?></p>           
               </div>
               <div class="hero-sub__img"> 
-                <?php if(is_category('news') || $category_nicename === 'news'):  ?>      
-                <img src="<?= get_template_directory_uri(); ?>/images/sub-topimage06.png" alt="ヘッダー画像" />  
-                <?php elseif(is_category('works') || $category_nicename === 'works'): ?>
-                <img src="<?= get_template_directory_uri(); ?>/images/sub-topimage06.png" alt="ヘッダー画像" />  
+                <?php if(is_category('news') || in_category('news')):  ?>      
+                <img src="<?= get_template_directory_uri(); ?>/images/subtop-image01.png" alt="ヘッダー画像" />  
+                <?php elseif(is_category('works') || in_category('works') ): ?>
+                <img src="<?= get_template_directory_uri(); ?>/images/subtop-image07.png" alt="ヘッダー画像" />  
              <?php endif; ?>
             </div>     
         </div>
       </div>
-      <?php elseif(is_page()): ?>
+      <?php elseif(is_single()):?>
+    <?php 
+    $category = get_the_category();
+    $cat_slug = $category[0]->category_nicename;
+    $cat_name = $category[0]->name;
+?>
+      <div class="hero-sub">    
+        <div class="hero-sub__inner">
+              <div class="hero-sub__titles">               
+              <h2 class="hero-sub__maintitle "><?=  esc_html($cat_slug); ?></h2>
+              <p class="hero-sub__subtitle"><?=  esc_html($cat_name); ?></p>           
+              </div>
+              <div class="hero-sub__img"> 
+                <?php if(is_category('news') || in_category('news')):  ?>      
+                <img src="<?= get_template_directory_uri(); ?>/images/subtop-image01.png" alt="ヘッダー画像" />  
+                <?php elseif(is_category('works') || in_category('works') ): ?>
+                <img src="<?= get_template_directory_uri(); ?>/images/subtop-image07.png" alt="ヘッダー画像" />  
+             <?php endif; ?>
+            </div>     
+        </div>
+      </div>
+      <?php elseif(is_page()) : ?>
+        <?php
+$page = get_post( get_the_ID() );
+$slug = $page->post_name;
+?>
         <div class="hero-sub">    
         <div class="hero-sub__inner">
               <div class="hero-sub__titles">
               <h2 class="hero-sub__maintitle "><?= strtoupper($post->post_name); ?></h2>
               <p class="hero-sub__subtitle"><?php the_title(); ?></p>
               </div>
-              <div class="hero-sub__img"> 
-                   
-              
-                <img src="<?= get_template_directory_uri(); ?>/images/sub-topimage06.png" alt="ヘッダー画像" />  
-    
+              <div class="hero-sub__img">    
+             <?php if($slug === "company"): ?>
+        <img src="<?= get_template_directory_uri(); ?>/images/subtop-image06.png" alt="ヘッダー画像" />  
+        <?php elseif($slug === "recruit"): ?>
+        <img src="<?= get_template_directory_uri(); ?>/images/subtop-image02.png" alt="ヘッダー画像" />  
+        <?php elseif($slug === "contact" || $slug === "thanks"): ?>
+        <img src="<?= get_template_directory_uri(); ?>/images/subtop-image03.png" alt="ヘッダー画像" />  
+        <?php elseif($slug === "privacy"): ?>
+        <img src="<?= get_template_directory_uri(); ?>/images/subtop-image05.png" alt="ヘッダー画像" />  
+        <?php elseif($slug === "sitemap"): ?>
+        <img src="<?= get_template_directory_uri(); ?>/images/subtop-image04.png" alt="ヘッダー画像" />  
+       <?php endif; ?>
             </div>     
         </div>
       </div>
-      <?php endif; ?>
+      <?php elseif(is_404()): ?>
+        <div class="hero-sub">    
+        <div class="hero-sub__inner">
+              <div class="hero-sub__titles">
+              <h2 class="hero-sub__maintitle ">404 NOT FOUND</h2>
+              </div>
+              <div class="hero-sub__img">        
+        <img src="<?= get_template_directory_uri(); ?>/images/sub-topimage04.png" alt="ヘッダー画像" />  
+            </div>     
+        </div>
+      </div>
+
+
+        <?php endif; ?>
