5f0b6a05f5229dae5919dcefa1560b0d32646437
神園巧真 <>
2023-03-29 16:32:08 +0900

    トップの投稿一覧のappearの動きを変更

--- kawahara-kensetsu/include/news.php
+++ kawahara-kensetsu/include/news.php
@@ -14,7 +14,7 @@
                      $the_query = new WP_Query($args);
                     ?>
                   <?php if($the_query->have_posts()): ?>                    
-                  <ul class="news__list appear left"> 
+                  <ul class="news__list appear right"> 
               <?php while($the_query->have_posts()):$the_query->the_post(); ?>
             <?php get_template_part('include/news-inside'); ?>
             <?php endwhile; ?>
