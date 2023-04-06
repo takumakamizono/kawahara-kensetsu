7be57b96a4f4b0997cb05fed9ff5f715593542e2
神園巧真 <>
2023-04-06 11:11:18 +0900

    アーカイブページのカテゴリ一覧の作成。

---
+++ kawahara-kensetsu/include/news-categories.php
@@ -0,0 +1,20 @@
+<?php 
+ $args =[
+       'parent' => 0,
+       'hide_empty' => false  //投稿無記事のカテゴリも表示させる   
+         ];
+$cats = get_categories($args);
+?>
+             <div class="news-cate">  
+                <p clss="sub-title">カテゴリー一覧</p>                    
+                <ul class="news-cate__ul appear up">
+                <?php foreach($cats as $cat ): ?>            
+                   <?php
+                     $cat_id = $cat->cat_ID;                     
+                     $cat_title = $cat->name;
+                     $cat_url = get_term_link($cat->term_id);
+                     ?>   
+                <li class="<?php if(is_category($cat_id)){ echo 'current'; } ?> item"><a class="btn shadow " href="<?= esc_url($cat_url); ?>"><?= esc_html($cat_title); ?></a></li>      
+                    <?php endforeach; ?>             
+                   </ul>               
+              </div> 
