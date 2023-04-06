894f4cbd79e70017f1f77955fe242da55bf51266
神園巧真 <>
2023-04-06 11:06:19 +0900

    トップページのお知らせ一覧の表示制限の修正と　ボタンのリンク先の修正。

--- kawahara-kensetsu/include/news.php
+++ kawahara-kensetsu/include/news.php
@@ -8,8 +8,8 @@
                 <?php
                      $args=[
                        'post_type'=> 'post',
-                       'posts_per_page' => 5,
-                       
+                       'category_name' => 'works,news',
+                       'posts_per_page' => 5,        
                      ];
                      $the_query = new WP_Query($args);
                     ?>
@@ -19,14 +19,23 @@
             <?php get_template_part('include/news-inside'); ?>
             <?php endwhile; ?>
                   </ul>
-           <?php endif; ?>
-             <?php wp_reset_postdata(); ?>                
-                </div>
-                <div class="news__btn appear up">
+                  <div class="news__btn appear up">
                 <?php
                       $news = get_term_by('slug','news','category');
                       $news_link = get_term_link($news,'category')
                       ?>
                     <a href="<?= esc_url($news_link); ?>"  class="btn slide-bg item">more</a>
                   </div>
+                  <?php else: ?>
+                    <div class="notice__notinfo">
+                      <p>新しい情報はありません</p>
+                     
+                      <div class="notice__btn">
+                      <a class="btn slide-bg" href="<?= esc_url(home_url('/')); ?>">トップページへ戻る</a> 
+                      </div>
+                    </div>
+           <?php endif; ?>
+             <?php wp_reset_postdata(); ?>                
+                </div>
+            
               </section>
